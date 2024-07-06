9423d1fb05d5d9a4cce46bb309956c683f06414d
InquisitorFury <studentaccahmed@gmail.com>
2024-07-06 20:18:01 +0100

    creating rest_framework

---
+++ src/paint/api/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from ..models import Digit
+
+class DigitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Digit
+        fields = ('id','image', 'result')
--- src/src/urls.py
+++ src/src/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('paint.urls')),
+    path('api', include('paint.api.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
 
